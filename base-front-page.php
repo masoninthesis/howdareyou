ea8776839beb434128406bedcb4650767563b4e3
Mason L <mason.lawlor@me.com>
2014-08-02 18:49:50 -0700

    updated optin,

---
+++ base-front-page.php
@@ -0,0 +1,175 @@
+<?php get_template_part('templates/head'); ?>
+<body class="body-bottom" data-spy="scroll" data-target=".navbar">
+
+  <!--[if lt IE 8]>
+    <div class="alert alert-t4tg">
+      <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'roots'); ?>
+    </div>
+  <![endif]-->
+
+<header class="banner navbar navbar-default navbar-fixed-top" role="banner">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="<?php echo home_url(); ?>/"><img src="http://masonlawlor.com//smartkit/assets/images/logo.png" alt="logo-nav" width="auto" height="48px" /></a>
+    </div>
+
+    <nav class="collapse navbar-collapse" role="navigation">
+    <button type="button" class="pull-right btn btn-default security-btn">Back on Indiegogo</button>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a title="Lightning fast" href="#home" class="security-nav scroll">Home Security</a></li>
+      <li><a title="Get in touch!" href="#energy" class="scroll energy-nav">Smart Energy</a></li>
+      <li><a title="External page sample" href="#auto" class="scroll auto-nav">Home Automation</a></li>
+    </ul>
+    </nav>
+  </div>
+</header>
+
+
+  <section id="above-the-fold">
+  	<video id="videobcg" preload="auto" autoplay="true" loop="loop" muted="muted" volume="0">
+     		<source src="http://masonlawlor.com//smartkit/assets/video/timelapse.mp4" type="video/mp4">
+     		<source src="http://masonlawlor.com//smartkit/assets/video/timelapse.ogv" type="video/ogv">
+          Sorry, your browser does not support HTML5 video.
+	</video>
+    <div class="wrap container" role="document">
+      <div class="content row">
+        <div class="col-sm-12">
+          <h1>No Contracts.</h1>
+          <h3>How's that for a <span class="smart-home">Smart Home?</span></h3>
+          <div class="col-sm-4 jack-cols">
+              <a class="hero-sections" href="#"><img src="http://masonlawlor.com//smartkit/assets/images/security-hero.png" alt="the-athlete" />
+              <h4>Security</h4></a>
+          </div>
+          <div class="col-sm-4 jack-cols">
+              <a class="hero-sections" href="#"><img src="http://masonlawlor.com//smartkit/assets/images/energy-hero.png" alt="the-fit" />
+              <h4>Energy</h4></a>
+          </div>
+          <div class="col-sm-4 jack-cols">
+              <a class="hero-sections" href="#"><img src="http://masonlawlor.com//smartkit/assets/images/automation-hero.png" alt="the-restored" />
+              <h4>Automation</h4></a>
+          </div>
+          <center></center>
+          <a href="//fast.wistia.net/embed/iframe/1se5qohbsh?popover=true" class="wistia-popover[height=360,playerColor=7b796a,width=640]"><button type="button" class="btn btn-default hero-btn">Play Video</button></a>
+<script charset="ISO-8859-1" src="//fast.wistia.com/assets/external/popover-v1.js"></script>
+        </div>
+      </div><!-- /.content -->
+    </div><!-- /.wrap -->
+  </section>
+  
+  
+
+  <section id="home" class="home jack-things">
+    <div class="wrap container" role="document">
+      <div class="content row">
+        <div class="col-sm-6"> 
+          <h2><img class="security-icon" src="http://masonlawlor.com/smartkit/assets/images/security.png" /> Home Security</h2>
+		    <ul class="big-list">
+	          <li><img class="icons" src="http://masonlawlor.com/smartkit/assets/images/1-design.png"/> Perimeter Protection</li>
+	          <li><img class="icons" src="http://masonlawlor.com/smartkit/assets/images/2-dev.png"/> Motion Detection</li>
+	          <li><img class="icons" src="http://masonlawlor.com/smartkit/assets/images/3-conversions.png"/> Smoke & CO Detection</li>
+	        </ul>
+          <div class="alert alert-t4tg">Starting at <span class="price-security">$34.99</span>/m</div>
+          <button class="btn btn-default btn-lg security-btn">Pre-order <i class="fa fa-chevron-right"></i></button>
+        </div>
+        <div class="col-sm-6">
+          <center>
+          <img class="art img-responsive" src="http://masonlawlor.com/smartkit/assets/images/graphics/security.png" />
+          </center>
+        </div>
+      </div><!-- /.content -->
+    </div><!-- /.wrap -->
+  </section>
+
+  <section id="energy" class="highlight home jack-things">
+    <div class="wrap container" role="document">
+      <div class="content row">
+        <div class="col-sm-6">
+          <h2><img class="energy-icon" src="http://masonlawlor.com/smartkit/assets/images/energy.png" /> Smart Energy</h2>
+          <ul class="big-list">
+	          <li><img class="icons" src="http://masonlawlor.com/smartkit/assets/images/1-design.png"/> Thermostat Control</li>
+	          <li><img class="icons" src="http://masonlawlor.com/smartkit/assets/images/2-dev.png"/> Light Control</li>
+	          <li><img class="icons" src="http://masonlawlor.com/smartkit/assets/images/4-templates.png"/> Energy Saving Schedules</li>
+	        </ul>
+          <div class="alert alert-t4tg">Starting at <span class="price-energy">$42.99</span>/m</div>
+          <button class="btn btn-default btn-lg energy-btn">Pre-order  <i class="fa fa-chevron-right"></i></button>
+        </div>
+        <div class="col-sm-6">
+          <center>
+          <img class="art img-responsive" src="http://masonlawlor.com/smartkit/assets/images/graphics/energy.png" />
+          </center>
+        </div>
+      </div><!-- /.content -->
+    </div><!-- /.wrap -->
+  </section>
+
+  <section id="auto" class="home jack-things">
+    <div class="wrap container" role="document">
+      <div class="content row">
+        <div class="col-sm-6">
+          <h2><img class="auto-icon" src="http://masonlawlor.com/smartkit/assets/images/automation.png" /> Home Automation</h2>
+          <ul class="big-list">
+	          <li><img class="icons" src="http://masonlawlor.com/smartkit/assets/images/1-design.png"/> Door Lock Control</li>
+	          <li><img class="icons" src="http://masonlawlor.com/smartkit/assets/images/3-conversions.png"/> Mood Presets</li>
+	          <li><img class="icons" src="http://masonlawlor.com/smartkit/assets/images/4-templates.png"/> Wifi Surveillance</li>
+	        </ul>
+          <div class="alert alert-t4tg">Starting at <span class="price-auto">$56.99</span>/m</div>
+          <button class="btn btn-default btn-lg auto-btn">Pre-order  <i class="fa fa-chevron-right"></i></button>
+        </div>
+        <div class="col-sm-6">
+          <center>
+          <img class="art img-responsive" src="http://masonlawlor.com/smartkit/assets/images/graphics/auto.png" />
+          </center>
+        </div>
+      </div><!-- /.content -->
+    </div><!-- /.wrap -->
+  </section>
+  
+  <section id="bottom-cta" class="home">
+    <div class="wrap container" role="document">
+      <div class="content row">
+        <div class="col-sm-12">
+	          <h1>The new definition of Smart Home.</h1>
+          <h3>Pre-order your <span class="smart-home">SmartKit</span> on Indiegogo</h3>
+          <center><button type="button" class="btn btn-default security-btn">Back on Indiegogo</button></center>
+          <center><a href="" data-toggle="modal" data-target="#myModal">Virtual Walk-thru: How to pick the right equipment for your home</a></center>
+          
+        </div>
+        <i class="fa fa-check"></i>
+      </div><!-- /.content -->
+    </div><!-- /.wrap -->
+  </section>
+
+
+  <?php get_template_part('templates/footer'); ?>
+  
+  <!-- pop-up modal -->
+
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+            <center><h3 class="modal-title" id="myModalLabel"><span class="smart-home">VIDEO TRAINING LIBRARY</span></h3></center>
+          </div>
+          <div class="modal-body">
+            <center><h3>Equip yourself like a pro</h3><br>
+            <p>Over the next 5 days you'll receive 5 videos, giving you the expertise to simplify your life and protect the things you love.</p></center>
+            <br />
+            <center><form class="form-inline" role="form">
+                <input class="required" style="font-style:italic;" placeholder=" your email ..."></input>
+                <button type="submit" class="submit btn" value="download">Send it my way</button>
+            </form></center><br>
+          </div>
+        </div>
+      </div>
+    </div><!-- /pop-up modal -->
+    
+	
+	</body>
+</html>
--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -21,69 +21,67 @@
 </header>
 
 <div class="wrap container">
-  <div class="row">
-    <center>
-    <div style="width: 80%;">
-      <iframe onClick="ga('send', 'event', 'video', 'play', 'main video');" src="//fast.wistia.net/embed/iframe/4j775txdig?videoFoam=true" allowtransparency="true" frameborder="0" scrolling="no" class="wistia_embed" name="wistia_embed" allowfullscreen mozallowfullscreen webkitallowfullscreen oallowfullscreen msallowfullscreen width="668" height="376"></iframe>
-      <script src='//fast.wistia.com/static/iframe-api-v1.js'></script>
+  <div class="col-sm-10"><!-- Video -->
+	  <div class="row"> 
+	    <center>
+	      <iframe onClick="ga('send', 'event', 'video', 'play', 'main video');" src="//fast.wistia.net/embed/iframe/4j775txdig?videoFoam=true" allowtransparency="true" frameborder="0" scrolling="no" class="wistia_embed" name="wistia_embed" allowfullscreen mozallowfullscreen webkitallowfullscreen oallowfullscreen msallowfullscreen width="668" height="376"></iframe>
+	      <script src='//fast.wistia.com/static/iframe-api-v1.js'></script>
+	    </center>
     </div>
-    </center>
+  </div> <!-- /Video -->
     
-    <div class="alert-header ">
-    <div class="col-sm-12">
-      <center>
-      <h4>Subscribe and I'll share the stories of how myself and others have dared.</h4>
-      </center>
-    </div>
-<div class="wrap container">
-  <div class="row">
-    <div class="col-sm-9 col-center">
-      <div class="top-optin">
-
-        <!-- AWeber Web Form Generator 3.0 -->
-        <form id="before_header" method="post" class="form-inline" action="http://www.aweber.com/scripts/addlead.pl"  >
-        <div style="display: none;">
-        <input type="hidden" name="meta_web_form_id" value="2074084490" />
-        <input type="hidden" name="meta_split_id" value="" />
-        <input type="hidden" name="listname" value="foreverjobless" />
-        <input type="hidden" name="redirect" value="http://www.aweber.com/thankyou.htm?m=default" id="redirect_f916c5c4ae42ade190efaef62fd11c16" />
-
-        <input type="hidden" name="meta_adtracking" value="FJ:_Header" />
-        <input type="hidden" name="meta_message" value="1" />
-        <input type="hidden" name="meta_required" value="email" />
-
-        <input type="hidden" name="meta_tooltip" value="" />
-        </div>
-        <!-- <div class="af-element"> -->
-        <input class="form-control" id="awf_field-60198198" type="text" name="email" value="" tabindex="500" style="width: 75%;"  />
-        <!-- </div> -->
-        <button id="header-optin-click" name="submit" onClick="ga('send', 'event', 'optin', 'subscribe', 'header optin');" class="submit btn btn-primary btn-lg" type="submit" value="Submit" tabindex="501">Subscribe</button>
-        <div class="af-clear"></div>
-        </div>
-        </div>
-        <div style="display: none;"><img src="http://forms.aweber.com/form/displays.htm?id=TAzsLAwcLCycDA==" alt="" /></div>
-        </form>
-        
-        <script language="JavaScript">
-
-        <!--
-        function _submit_form_header(frm){
-          var email = frm.email.value;
-          var valid_email = /^([a-zA-Z0-9_.-])+@(([a-zA-Z0-9-])+.)+([a-zA-Z0-9]{2,4})+$/;
-          if( valid_email.test( email ) ){
-            document.getElementById('before_header').style.display = 'none';
-            document.getElementById('after_header').style.display = 'block';
-            return true;
-          } else {
-            alert("Invalid email address");
-          }
-          return false;
-        }
-        //-->
-        </script>
-        <!-- /AWeber Web Form Generator 3.0 -->
-
+  <div class="alert-header col-sm-10"><!-- Optin -->
+      <div class="row">
+        <h4 class="optin-h4">Subscribe and I'll share the stories of how myself and others have dared.</h4>
       </div>
+	  <div class="row">
+	    <div class="col-sm-9 col-center">
+	      <div class="top-optin">
+	
+	        <!-- AWeber Web Form Generator 3.0 -->
+	        <form id="before_header" method="post" class="form-inline" action="http://www.aweber.com/scripts/addlead.pl"  >
+	        <div style="display: none;">
+	        <input type="hidden" name="meta_web_form_id" value="2074084490" />
+	        <input type="hidden" name="meta_split_id" value="" />
+	        <input type="hidden" name="listname" value="foreverjobless" />
+	        <input type="hidden" name="redirect" value="http://www.aweber.com/thankyou.htm?m=default" id="redirect_f916c5c4ae42ade190efaef62fd11c16" />
+	
+	        <input type="hidden" name="meta_adtracking" value="FJ:_Header" />
+	        <input type="hidden" name="meta_message" value="1" />
+	        <input type="hidden" name="meta_required" value="email" />
+	
+	        <input type="hidden" name="meta_tooltip" value="" />
+	        </div>
+	        <!-- <div class="af-element"> -->
+	        <input class="form-control" id="awf_field-60198198" type="text" name="email" value="" tabindex="500" style="width: 75%;"  />
+	        <!-- </div> -->
+	        <button id="header-optin-click" name="submit" onClick="ga('send', 'event', 'optin', 'subscribe', 'header optin');" class="submit btn btn-primary btn-lg" type="submit" value="Submit" tabindex="501">Subscribe</button>
+	        <div class="af-clear"></div>
+	        </div>
+	        </div>
+	        <div style="display: none;"><img src="http://forms.aweber.com/form/displays.htm?id=TAzsLAwcLCycDA==" alt="" /></div>
+	        </form>
+	        
+	        <script language="JavaScript">
+	
+	        <!--
+	        function _submit_form_header(frm){
+	          var email = frm.email.value;
+	          var valid_email = /^([a-zA-Z0-9_.-])+@(([a-zA-Z0-9-])+.)+([a-zA-Z0-9]{2,4})+$/;
+	          if( valid_email.test( email ) ){
+	            document.getElementById('before_header').style.display = 'none';
+	            document.getElementById('after_header').style.display = 'block';
+	            return true;
+	          } else {
+	            alert("Invalid email address");
+	          }
+	          return false;
+	        }
+	        //-->
+	        </script>
+	        <!-- /AWeber Web Form Generator 3.0 -->
+	
+	      </div>
       </div>
     </div>
   </div>
