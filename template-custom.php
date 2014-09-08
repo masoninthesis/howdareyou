258ea46042e73c3e54b95dd530cc2092fb8344d9
Mason L <mason.lawlor@me.com>
2014-09-07 18:16:47 -0700

    Posting Schedule Added

--- template-custom.php
+++ template-custom.php
@@ -6,13 +6,73 @@ Template Name: HDY Homepage
 
 <?php get_template_part('templates/page', 'header'); ?>
 
-<br />
+
+
+<h1><i class="fa fa-info-circle vert-top"></i> Content Schedule</h1><br>
+	<div class="row icons content-sched">
+		<div class="col-md-2 col-sm-4 col-sm-6">
+			<div class="icon"><a href="#"><img src="http://masonlawlor.com//howdareyoudotio/wp-content/themes/howdareyou/assets/img/hdy-icon.png" alt="HowDareYou Post"></a></div>
+			<div class="btn-text"><h5>Sunday</h5></div> 
+		</div>
+		<div class="col-md-2 col-sm-4 col-sm-6">
+			<div class="icon"><a href="#"><img src="http://masonlawlor.com//howdareyoudotio/wp-content/themes/howdareyou/assets/img/smartkit-icon.png" alt="SmartKit Post"></a></div>
+			<div class="btn-text"><h5>Monday</h5></div> 
+		</div>
+		<div class="col-md-2 col-sm-4 col-sm-6">
+			<div class="icon"><a href="#"><img src="http://masonlawlor.com//howdareyoudotio/wp-content/themes/howdareyou/assets/img/crowdfundation-icon.png"></a></div>
+			<div class="btn-text" alt="CrowdFundation Post"><h5>Tuesday</h5></div> 
+		</div>
+		<div class="col-md-2 col-sm-4 col-sm-6">
+			<div class="icon"><a href="#"><img src="http://masonlawlor.com//howdareyoudotio/wp-content/themes/howdareyou/assets/img/jackalope-icon.png"></a></div>
+			<div class="btn-text" alt="Jackalope Post"><h5>Wednesday</h5></div> 
+		</div>
+		<div class="col-md-2 col-sm-4 col-sm-6">
+			<div class="icon"><a href="#"><img src="http://masonlawlor.com//howdareyoudotio/wp-content/themes/howdareyou/assets/img/hdy-icon.png" alt="HowDareYou Lifehack or Guest Post"></a></div>
+			<div class="btn-text"><h5>Thursday</h5></div> 
+		</div>
+		<div class="col-md-2 col-sm-4 col-sm-6">
+			<div class="icon"><a href="#"><img src="http://masonlawlor.com//howdareyoudotio/wp-content/themes/howdareyou/assets/img/jackalope-icon.png" alt="Jackalope Tutorial"></a></div>
+			<div class="btn-text"><h5>Friday</h5></div> 
+		</div>
+	</div>
+	
+<hr>
+
+<h1><i class="fa fa-info-circle vert-top"></i> Content Schedule</h1><br>
+	<div class="row icons">
+		<div class="col-md-2 col-sm-4 col-sm-6">
+			<div class="icon"><a href="#"><i class="fa fa-info-circle"></i></a></div>
+			<div class="btn-text"><h5>My Story</h5></div> 
+		</div>
+		<div class="col-md-2 col-sm-4 col-sm-6">
+			<div class="icon"><a href="#"><i class="fa fa-facebook"></i></a></div>
+			<div class="btn-text"><h5>Facebook</h5></div> 
+		</div>
+		<div class="col-md-2 col-sm-4 col-sm-6">
+			<div class="icon"><a href="#"><i class="fa fa-instagram"></a></div>
+			<div class="btn-text" alt="CrowdFundation Post"><h5>Instagram</h5></div> 
+		</div>
+		<div class="col-md-2 col-sm-4 col-sm-6">
+			<div class="icon"><a href="#"><i class="fa fa-spotify"></a></div>
+			<div class="btn-text" alt="Jackalope Post"><h5>Spotify</h5></div> 
+		</div>
+		<div class="col-md-2 col-sm-4 col-sm-6">
+			<div class="icon"><a href="#"><i class="fa fa-pinterest"></i></a></div>
+			<div class="btn-text"><h5>Pintrest</h5></div> 
+		</div>
+		<div class="col-md-2 col-sm-4 col-sm-6">
+			<div class="icon"><a href="#"><i class="fa fa-google-plus-square"></a></div>
+			<div class="btn-text"><h5>Google+</h5></div> 
+		</div>
+	</div>
+	
+<hr>
 
 <h1>Apps</h1>
 	<div class="row icons">
 		<div class="col-sm-1">
 			<a href="#" class="squircle"><i class="fa fa-info-circle"></i></a>
-			<span>My Story</span> 
+			<h5> Story</h5> 
 		</div>
 		<div class="col-sm-1">
 			<a href="#" class="squircle"><i class="fa fa-facebook"></i></a>
