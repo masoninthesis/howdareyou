744988093e341af59165fb21634f8548f7158e21
Mason L <mason.lawlor@me.com>
2014-09-17 18:55:15 -0700

    Bootup page added, error on js needs fix

---
+++ template-bootup.php
@@ -0,0 +1,280 @@
+<?php
+/*
+Template Name: Routine: Bootup
+*/
+?>
+
+<div class="page-header">
+	<h1><i class="fa fa-power-off"></i> Bootup <small>Daily Routine</small></h1>
+</div>
+
+<script type="text/javascript">
+	$('document').ready(function(){
+		$('#myToolTip1').tooltip();
+		$('#myToolTip2').tooltip();
+		$('#myToolTip3').tooltip();
+		$('#myToolTip4').tooltip();
+		$('#myToolTip5').tooltip();
+	});
+</script>
+
+<p></p>
+
+<div class="panel-group" id="accordion">
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
+          <i class="fa fa-info-circle"></i> Instructions
+        </a>
+      </h4>
+    </div>
+    <div id="collapseOne" class="panel-collapse collapse in">
+      <div class="panel-body">
+        Every day starts the same, I go to howdareyou.io and run my Bootup process. With Bootup, the point is to replicate the consistency of how your computer boots up. Your computer's bootup process is down to a science, and if you have a good computer (ahem, Mac), you're computer should bootup without fail 99.99% of the time. Bootup was my answer to making my routine a science. It's easy to get sidetracked with your day, but as long as I follow my Bootup I know that I should have a productive, energetic, and meaningful day. My process is constantly evolving.
+      </div>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
+          <i class="fa fa-sun-o"></i> Dream Journal
+        </a>
+      </h4>
+    </div>
+    <div id="collapseTwo" class="panel-collapse collapse">
+      <div class="panel-body">
+        <a href="#">My dream journal</a>. Every morning when I wake up, the first thing I do is open Evernote and try to put my dreams in writing. Sometimes it can be hard to remember your dreams at all, but I've found as long as you can remember a small portion of it you can remember more once you get writing. This journal is my gateway into my dreams. Sure, they seem nonsensical most of the time, but going back on old entries I can sometimes find profound meaning. Usually hinting to my subliminal intentions/fears. This is also a common technique to practicing lucid dreaming. If you learn to master this, it would make the future of VR look like child's play (I haven't had any luck mastering it yet).
+      </div>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
+          <i class="fa fa-ban"></i> NotToDo.app
+        </a>
+      </h4>
+    </div>
+    <div id="collapseThree" class="panel-collapse collapse">
+      <div class="panel-body">
+        <a href="#">My NotToDo list</a>. More important than what you do, is sometimes what you don't do. That's why I've placed my NotToDo list before my ToDo list. For example, maybe you have the habit of taking phone calls before you get to your ToDo list, which sidetracks you. You can set a reminder– don't take any phone calls before Bootup is complete. 
+      </div>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
+          <i class="fa fa-compass"></i> Affirmations.app
+        </a>
+      </h4>
+    </div>
+    <div id="collapseFour" class="panel-collapse collapse">
+      <div class="panel-body">
+        <a href="#">My affirmations</a>. Perspective is one of the most powerful forces in this life. There's what happens in life, and there's the story you tell about it. The story you tell about your life makes all the difference. Write a story that can have powerful transformation in yourself and tell it to yourself everyday. If you find yourself not believing it, it's time to reevaluate who you've become. 
+      </div>
+    </div>
+  </div>
+<div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
+          <i class="fa fa-facebook-square"></i> Facebook <span class="badge">5 min</span>
+        </a>
+      </h4>
+    </div>
+    <div id="collapseFive" class="panel-collapse collapse">
+      <div class="panel-body">
+        I've found that limiting my Facebook time to 5 minutes in the morning makes sure that I don't miss anything important. The point is to skim my news feed as quickly as possible. This forces me to be very selective about the information I am ingesting. I'm usually looking for only information that is immediately applicable to my life, or perhaps useful in the very near future. If I see something that piques my interest, I open it in Safari for later reading/indexing.
+      </div>
+    </div>
+  </div>
+<div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
+          <i class="fa fa-twitter-square"></i> Twitter <span class="badge">5 min</span>
+        </a>
+      </h4>
+    </div>
+    <div id="collapseSix" class="panel-collapse collapse">
+      <div class="panel-body">
+        I've found that limiting my Twitter time to 5 minutes in the morning makes sure that I don't miss anything important. The point is to skim my news feed as quickly as possible. This forces me to be very selective about the information I am ingesting. I'm usually looking for only information that is immediately applicable to my life, or perhaps useful in the very near future. If I see something that piques my interest, I open it in Safari for later reading/indexing.
+      </div>
+    </div>
+  </div>
+<div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
+          <i class="fa fa-hacker-news"></i> TheNews.im <span class="badge">5 min</span>
+        </a>
+      </h4>
+    </div>
+    <div id="collapseSeven" class="panel-collapse collapse">
+      <div class="panel-body">
+        I've found that limiting my Designer News/Hacker News time to 5 minutes in the morning makes sure that I don't miss anything important. The point is to skim my news feed as quickly as possible. This forces me to be very selective about the information I am ingesting. I'm usually looking for only information that is immediately applicable to my life, or perhaps useful in the very near future. If I see something that piques my interest, I open it in Safari for later reading/indexing.
+      </div>
+    </div>
+  </div>
+<div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
+          <i class="fa fa-envelope-square"></i> Email <span class="badge">5 min</span>
+        </a>
+      </h4>
+    </div>
+    <div id="collapseEight" class="panel-collapse collapse">
+      <div class="panel-body">
+        This is where I skim my email for 5 minutes to mark everything as read. Anything important but too long to skim in a matter of seconds is left for later, once my work day begins. I never reply to emails before my work day begins. If I have an irrelevant email, I immediately unsubscribe from it (It takes less than 30 seconds, and keeps digital noise to a min).
+      </div>
+    </div>
+  </div>
+<div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
+         <i class="fa fa-bookmark"></i> Bookmark/Index
+        </a>
+      </h4>
+    </div>
+    <div id="collapseNine" class="panel-collapse collapse">
+      <div class="panel-body">
+        This is where I separate my findings into two categories. I usually bookmark tutorials and resources into bookmark folders. Articles I add to my reading list for later use. I never read articles during this step. 
+      </div>
+    </div>
+  </div>
+<div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
+         <i class="fa fa-drupal"></i> Piss
+        </a>
+      </h4>
+    </div>
+    <div id="collapseTen" class="panel-collapse collapse">
+      <div class="panel-body">
+        I once heard that all geniuses pissed first thing in the morning. If you don't have to, you're not keeping hydrated enough. 
+      </div>
+    </div>
+  </div>
+<div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
+         <i class="fa fa-child"></i> 25 Pushups + 100 Bicycle Crunches
+        </a>
+      </h4>
+    </div>
+    <div id="collapseEleven" class="panel-collapse collapse">
+      <div class="panel-body">
+        Just to get the blood flowing.
+      </div>
+    </div>
+  </div>
+<div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
+         <i class="fa fa-th-list"></i> 5 Min Journal
+        </a>
+      </h4>
+    </div>
+    <div id="collapseTwelve" class="panel-collapse collapse">
+      <div class="panel-body">
+        This is my favorite behavioral programming app. It costs $2.99, but it's life changing. It's one of the few paid apps I own. Download <a href="#">here</a>.
+      </div>
+    </div>
+  </div>
+<div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
+         <i class="fa fa-apple"></i> Breakfast
+        </a>
+      </h4>
+    </div>
+    <div id="collapseThirteen" class="panel-collapse collapse">
+      <div class="panel-body">
+        I like to cook the same thing every morning: Bacon breakfast burritos. Eggs, sweet potatoes, bacon, and homemade salsa.
+      </div>
+    </div>
+  </div>
+<div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
+         <i class="fa fa-video-camera"></i> Vlog
+        </a>
+      </h4>
+    </div>
+    <div id="collapseFourteen" class="panel-collapse collapse">
+      <div class="panel-body">
+        It's been awhile since I did this, but I need to get in the habit of it again. Here was <a href="#">my first week</a>, it transformed my life. I will definitely be making this a regular thing in the future. It does require a lot of work to keep up on the editing.
+      </div>
+    </div>
+  </div>
+<div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
+         <i class="fa fa-check-square"></i> ToDo.app
+        </a>
+      </h4>
+    </div>
+    <div id="collapseFifteen" class="panel-collapse collapse">
+      <div class="panel-body">
+        I do my ToDo lists 1 of 2 ways. Some days I use the reminders app on my iPad, some days I do it with pen and a paper folder into quarters. I usually use the paper method when I'm editing/revising something. For example, I might look over a website I'm working on the night before, and write down all the things I want to fix/improve in list form.
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- pop-up modal -->
+<script type="text/javascript"
+        src="//cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.3.1/jquery.cookie.min.js"></script>
+  <script type="text/javascript">
+    function showPopup() {
+      $('#myModal').modal('show');
+    }
+
+    function timeOut() {
+      if ($.cookie('foreverjobless') !== '1') {
+        setTimeout(function() {
+          showPopup();
+          jQuery.cookie('foreverjobless', '1', { expires: 7 });
+        }, 400000);
+      }
+    }
+
+    $(document).ready(function () {
+      timeOut();
+      stickyElement();
+    });
+
+  </script>
+
+   <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+        	<center><h3>Get with the program.</h3></center>
+          	<div class="modal-body">
+          	<center><iframe src="//fast.wistia.net/embed/iframe/5tl43fy1m9?videoFoam=true" allowtransparency="true" frameborder="0" scrolling="no" class="wistia_embed" name="wistia_embed" allowfullscreen mozallowfullscreen webkitallowfullscreen oallowfullscreen msallowfullscreen width="640" height="360"></iframe><script src="//fast.wistia.net/assets/external/iframe-api-v1.js"></script></center>
+    			
+	      
+					      <!-- Mailchimp -->
+							<center><form class="form-inline" id="before_header" action="//smartk.us8.list-manage.com/subscribe/post?u=99a776394eb3b3549e7c5e53c&amp;id=6a60137cbc" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+								<input type="email" value="" name="EMAIL" class="form-control email" id="mce-EMAIL" placeholder="Email address.." required>
+							<button id="header-optin-click" name="submit" onClick="ga('send', 'event', 'optin', 'subscribe', 'header optin');" class="modal-btn btn btn-lg" type="submit" value="Submit" tabindex="501">Subscribe</button>
+							
+							</form></center> <!-- /Mailchimp -->
+	      </div><!-- /optin -->
+		  <br><br>
+          </div>
+        </div>
+      </div>
+    </div>
+<!-- /pop-up modal -->
