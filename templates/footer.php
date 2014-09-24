72c6864a124e6fa65cb6f65df2d35def2fc13aca
Mason L <mason.lawlor@me.com>
2014-09-24 15:14:37 -0700

    cleaned up footer- hopefully

--- templates/footer.php
+++ templates/footer.php
@@ -1,38 +1,25 @@
 <footer class="content-info" role="contentinfo">
-  <div class="upper-col">
-    <div class="wrap container">
-    	<div class="row">
-    		<div class="col-sm-5 pad">
-    			 <div class="col-sm-10 col-sm-offset-1"><!-- Video -->
-	  <div class="row"> 
-	    <center>
-	      <iframe onClick="ga('send', 'event', 'video', 'play', 'main video');" src="//fast.wistia.net/embed/iframe/5tl43fy1m9?videoFoam=true" allowtransparency="true" frameborder="0" scrolling="no" class="wistia_embed" name="wistia_embed" allowfullscreen mozallowfullscreen webkitallowfullscreen oallowfullscreen msallowfullscreen width="668" height="376"></iframe>
-	      <script src='//fast.wistia.com/static/iframe-api-v1.js'></script>
-	    </center>
-    </div>
-  </div> <!-- /Video -->
-    		</div>
-    		<div class="col-sm-7">
-          <div class="bottom-opt">
-      <!-- Optin -->
-      <div class="row">
-        <h4 class="optin-h4">Subscribe below and I'll dare you to be great.</h4>
-      </div>
-	  <div class="row">
-	      <div class="top-optin col-sm-offset-2">
-	      
-	      <!-- Mailchimp -->
-			<form class="form-inline" id="before_header" action="//howdareyou.us8.list-manage.com/subscribe/post?u=4c05f20503bc3a9d84d1a63c9&amp;id=488afa7633" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-			<input type="email" value="" name="EMAIL" class="form-control email" id="mce-EMAIL" placeholder="Email address.." required>
-			<button id="header-optin-click" name="submit" onClick="ga('send', 'event', 'optin', 'subscribe', 'header optin');" class="submit btn btn-primary btn-lg" type="submit" value="Submit" tabindex="501">I Dare</button>
-			
-			</form> <!-- /Mailchimp -->
-	      </div>
-      </div><!-- /optin --> 
-        </div>
-      </div>
-    </div>
-  </div>
+	<div class="upper-col">
+		<div class="wrap container">
+			<div class="row">
+				<div class="col-sm-5 pad"><!-- Video --> 
+					<center>
+						<iframe onClick="ga('send', 'event', 'video', 'play', 'main video');" src="//fast.wistia.net/embed/iframe/5tl43fy1m9?videoFoam=true" allowtransparency="true" frameborder="0" scrolling="no" class="wistia_embed" name="wistia_embed" allowfullscreen mozallowfullscreen webkitallowfullscreen oallowfullscreen msallowfullscreen width="668" height="376"></iframe>
+						<script src='//fast.wistia.com/static/iframe-api-v1.js'></script>
+					</center>
+				</div> <!-- /Video -->
+			</div>
+			<div class="col-sm-7"><!-- Optin -->
+				<div class="bottom-opt">
+					<h4 class="optin-h4">Subscribe below and I'll dare you to be great.</h4>
+					<!-- Mailchimp --><form class="form-inline" id="before_header" action="//howdareyou.us8.list-manage.com/subscribe/post?u=4c05f20503bc3a9d84d1a63c9&amp;id=488afa7633" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+					<input type="email" value="" name="EMAIL" class="form-control email" id="mce-EMAIL" placeholder="Email address.." required>
+					<button id="header-optin-click" name="submit" onClick="ga('send', 'event', 'optin', 'subscribe', 'header optin');" class="submit btn btn-primary btn-lg" type="submit" value="Submit" tabindex="501">I Dare</button>
+					</form> <!-- /Mailchimp -->
+				</div>
+			</div><!-- /optin --> 
+		</div>
+	</div>
   <div class="bottom-col">
     <div class="wrap container">
       <div class="row">
