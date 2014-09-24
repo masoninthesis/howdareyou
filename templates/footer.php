ab13754a01c01524a58787e90aa8c99ff85075ea
Mason L <mason.lawlor@me.com>
2014-09-24 15:27:22 -0700

    cleaned up footer

--- templates/footer.php
+++ templates/footer.php
@@ -8,36 +8,36 @@
 						<script src='//fast.wistia.com/static/iframe-api-v1.js'></script>
 					</center>
 				</div> <!-- /Video -->
+				<div class="col-sm-7"><!-- Optin -->
+					<div class="bottom-opt">
+						<h4>Subscribe below and I'll dare you to be great.</h4>
+						<!-- Mailchimp --><form class="form-inline" id="before_header" action="//howdareyou.us8.list-manage.com/subscribe/post?u=4c05f20503bc3a9d84d1a63c9&amp;id=488afa7633" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+						<input type="email" value="" name="EMAIL" class="form-control email" id="mce-EMAIL" placeholder="Email address.." required>
+						<button id="header-optin-click" name="submit" onClick="ga('send', 'event', 'optin', 'subscribe', 'header optin');" class="submit btn btn-primary btn-lg" type="submit" value="Submit" tabindex="501">I Dare</button>
+						</form> <!-- /Mailchimp -->
+					</div>
+				</div><!-- /optin --> 
 			</div>
-			<div class="col-sm-7"><!-- Optin -->
-				<div class="bottom-opt">
-					<h4 class="optin-h4">Subscribe below and I'll dare you to be great.</h4>
-					<!-- Mailchimp --><form class="form-inline" id="before_header" action="//howdareyou.us8.list-manage.com/subscribe/post?u=4c05f20503bc3a9d84d1a63c9&amp;id=488afa7633" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-					<input type="email" value="" name="EMAIL" class="form-control email" id="mce-EMAIL" placeholder="Email address.." required>
-					<button id="header-optin-click" name="submit" onClick="ga('send', 'event', 'optin', 'subscribe', 'header optin');" class="submit btn btn-primary btn-lg" type="submit" value="Submit" tabindex="501">I Dare</button>
-					</form> <!-- /Mailchimp -->
-				</div>
-			</div><!-- /optin --> 
 		</div>
 	</div>
-  <div class="bottom-col">
-    <div class="wrap container">
-      <div class="row">
-        <div class="col-sm-12 bottom">
-          <fieldset>
-            <ul>
-              <li><a class="footer-links" href="#">SmartKit</a></li> 
-              <li><a class="footer-links" href="#">ML Design</a></li> 
-              <li><a class="footer-links" href="#">My Story</a></li> 
-              <li><a class="footer-links" href="#">Daily Dares</a></li> 
-            </ul>
-          </fieldset>
-          <br />
-          <center><img src="http://masonlawlor.com/HDY/imgs/Footer-48.png" /></center>
-        </div>
-      </div>
-    </div>
-  </div>
+	<div class="bottom-col">
+		<div class="wrap container">
+			<div class="row">
+				<div class="col-sm-12 bottom">
+					<fieldset>
+						<ul>
+							<li><a class="footer-links" href="#">SmartKit</a></li> 
+							<li><a class="footer-links" href="#">Jackalope</a></li> 
+							<li><a class="footer-links" href="#">My Story</a></li> 
+							<li><a class="footer-links" href="#">Daily Dares</a></li> 
+						</ul>
+					</fieldset>
+					<br />
+					<center><img src="http://masonlawlor.com/HDY/imgs/Footer-48.png" /></center>
+				</div>
+			</div>
+		</div>
+	</div>
 </footer>
   		
 
